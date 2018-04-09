76dac74d4a7d922be8c8757f0cf317f26bceedee
DevastaingDj <sharma.dj1998@gmail.com>
2018-04-09 18:34:19 +0530

    Smoking type select

--- ReservationSystem/src/reservationsystem/SecondFrame.java
+++ ReservationSystem/src/reservationsystem/SecondFrame.java
@@ -7,9 +7,11 @@ package reservationsystem;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -22,36 +24,10 @@ import javax.swing.SwingConstants;
  * @author DSharma
  */
 
-//class RadioButtonExample extends JFrame implements ActionListener {    
-//JRadioButton rb1,rb2;    
-//JButton b;    
-//RadioButtonExample(){      
-//rb1=new JRadioButton("Male");    
-//rb1.setBounds(100,50,100,30);      
-//rb2=new JRadioButton("Female");    
-//rb2.setBounds(100,100,100,30);    
-//ButtonGroup bg=new ButtonGroup();    
-//bg.add(rb1);bg.add(rb2);    
-//b=new JButton("click");    
-//b.setBounds(100,150,80,30);    
-//b.addActionListener(this);    
-//add(rb1);add(rb2);add(b);    
-//setSize(300,300);    
-//setLayout(null);    
-//setVisible(true);    
-//}    
-//public void actionPerformed(ActionEvent e){    
-//if(rb1.isSelected()){    
-//JOptionPane.showMessageDialog(this,"You are Male.");    
-//}    
-//if(rb2.isSelected()){    
-//JOptionPane.showMessageDialog(this,"You are Female.");    
-//}    
-//}    
-
 class SecondPanel extends JPanel implements ActionListener {
     JRadioButton rb1,rb2;
     public SecondPanel(){
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         JLabel label = new JLabel("Choose a Type: ", SwingConstants.CENTER);
         add(label);
         setSize(600, 600);
@@ -61,24 +37,47 @@ class SecondPanel extends JPanel implements ActionListener {
         rb2.setBounds(200,200,100,30);    
         ButtonGroup bg=new ButtonGroup();    
         bg.add(rb1);bg.add(rb2);  
-        JButton b=new JButton("Proceed");    
-        b.setBounds(200,250,80,30);
-        b.addActionListener(this);
-        add(rb1);add(rb2);add(b);
+        JButton proceed = new JButton("Proceed");    
+        proceed.setBounds(200,250,80,30);
+        proceed.addActionListener(this);
+        add(rb1);add(rb2);add(proceed);
     }
-
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         if(rb1.isSelected()){
-            System.exit(0);
+            
         }    
         if(rb2.isSelected()){    
-            System.exit(0);
+          repaint();
     }
  }
 }
+class ThirdPanel extends JPanel {
+        public ThirdPanel(){
+            setLayout(new GridLayout(2,5));
+            JButton b1=new JButton("1");
+            b1.setBounds(100,50,20,20);
+            JButton b2=new JButton("2");  
+            JButton b3=new JButton("3");  
+            JButton b4=new JButton("4");  
+            JButton b5=new JButton("5");  
+            JButton b6=new JButton("6");  
+            JButton b7=new JButton("7");  
+            JButton b8=new JButton("8");  
+            JButton b9=new JButton("9");
+            JButton b10=new JButton("10");
+            add(b1);add(b2);
+            add(b3);add(b4);
+            add(b5);add(b6);
+            add(b7);add(b8);
+            add(b9);add(b10);
+        }
+}
+
 public class SecondFrame extends JFrame {
-    private SecondPanel secondPanel;
+     SecondPanel secondPanel;
+     ThirdPanel thirdPanel;
     public SecondFrame() {
         setVisible(true);
         setTitle("Winchester Airlines Booking Portal");
@@ -89,6 +88,8 @@ public class SecondFrame extends JFrame {
         setLocation(s.width/2 - getWidth()/2,s.height/2 - getHeight()/2);
         secondPanel = new SecondPanel();
         add(secondPanel);
+        thirdPanel = new ThirdPanel();
+        //add(thirdPanel);
         
     }
     
